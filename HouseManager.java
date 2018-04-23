be2ab3e246d29c12cfcc3fdd22a3fdf6b8ab7487
21nadkarnid <38667595+21nadkarnid@users.noreply.github.com>
2018-04-23 14:17:22 -0400

    Update HouseManager.java

--- HouseManager.java
+++ HouseManager.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 public class HouseManager{
-  ArrayList<House> houses = new ArrayList<House>();
+  private ArrayList<House> houses = new ArrayList<House>();
   public HouseManager(House h){
     houses.add(h);
+  }
+  public ArrayList<House> getList(){
+    return houses;
   }
 }
