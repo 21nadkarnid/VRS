c4f8a0b342bf1eba0af940d790abdbe49cb9a5ae
zeroman29 <38667592+zeroman29@users.noreply.github.com>
2018-04-24 14:27:04 -0400

    Update Neighbor.java

--- Neighbor.java
+++ Neighbor.java
@@ -1,5 +1,11 @@
 public class Neighbor {
   public HouseManager NN(HouseManager path) {
     House home = new House(125, 22, a);
+    House current = home;
+    int minDist = 65536;
+    for (int i = 0; i < path.getList().size(); i++) {
+      if (path.getList().get(i).distanceTo(current) < minDist) {
+        
+    }
   }
 }
