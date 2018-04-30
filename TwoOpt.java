9c785936934ec049c578c69ad628683cb6da2184
zeroman29 <38667592+zeroman29@users.noreply.github.com>
2018-04-30 14:10:50 -0400

    Update TwoOpt.java

--- TwoOpt.java
+++ TwoOpt.java
@@ -3,7 +3,20 @@ public class TwoOpt {
    HouseManager path = Neighbor.NN(input);
    boolean done = false;
    while (!done) {
-     for (int i = 0; i < 
+     done = true;
+     int dist = path.totDistance();
+     int[] best = new int[2];
+     for (int i = 1; i < path.getList().size(); i++) {
+       for (int j = 0; j < i; j++) {
+         if (path.switchD(j, i) < dist) {
+           best[0] = j;
+           best[1] = i;
+           done = false;
+         }
+       }
+     }
+     path.switch(j, i);
    }
+   return path;
   }
 }
