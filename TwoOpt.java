7615cab3cd525a257711559e62fa6a8adfe33dab
zeroman29 <38667592+zeroman29@users.noreply.github.com>
2018-04-30 13:56:12 -0400

    Update TwoOpt.java

--- TwoOpt.java
+++ TwoOpt.java
@@ -1,5 +1,9 @@
 public class TwoOpt {
   public HouseManager TO (HouseManager input) {
-   HouseManager path =  
+   HouseManager path = Neighbor.NN(input);
+   boolean done = false;
+   while (!done) {
+     for (int i = 0; i < 
+   }
   }
 }
