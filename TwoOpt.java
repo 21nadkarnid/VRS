7fb10f94290cec72c1d46c44ef3cc777ab1a1fa2
zeroman29 <38667592+zeroman29@users.noreply.github.com>
2018-05-02 14:19:49 -0400

    Update TwoOpt.java

--- TwoOpt.java
+++ TwoOpt.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class TwoOpt {
   public HouseManager TO (HouseManager input) {
-   HouseManager path = Neighbor.NN(input);
+   HouseManager path = TwoOpt.NN(input);
    boolean done = false;
    while (!done) {
      done = true;
@@ -19,4 +21,26 @@ public class TwoOpt {
    }
    return path;
   }
+  public HouseManager NN(HouseManager list, House complex) {
+    House home = new House(125, 22, 'a');
+    House current = home;
+    ArrayList<House> path = new ArrayList<House>();
+    boolean[] delivered = new boolean[list.getList().size()];
+    for (int i = 0; i < list.getList().size(); i++) {
+      int currentBest = -1;
+      int minDist = 65536;
+      int j;
+      for (j = 0; j < list.getList().size(); j++) {
+        if (!delivered[j] && list.getList().get(j).distanceTo(current) < minDist) {
+          minDist = list.getList().get(j).distanceTo(current);
+          currentBest = j;
+        }
+      }
+      current = list.getList().get(j);
+      path.add(current);
+      delivered[j] = true;
+    }
+    return new HouseManager(path);
+  }
+
 }
